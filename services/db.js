ed4a2cdf354b72c98e7f1a6ca3b8112d78d4bc59
ykokw <0k4w4.yuk1@gmail.com>
2018-12-13 14:29:48 +0900

    Refactor services

--- services/db.js
+++ services/db.js
@@ -1,16 +1,6 @@
-import firebase from 'firebase';
 import 'firebase/firestore';
 
-const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-};
-
-firebase.initializeApp(config);
+import firebase from './firebase';
 
 const dbSettings = { timestampsInSnapshots: true };
 
---
+++ services/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+};
+
+export default firebase.initializeApp(config);
