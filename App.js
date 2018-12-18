ee05b2e665668ac87dcfe084f5e27143bd96ab8c
ykokw <0k4w4.yuk1@gmail.com>
2018-12-18 10:36:46 +0900

    Fixed navigation

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, createSwitchNavigator } from "react-navigation";
 
 import HomeScreen from "./screens/Home";
 import ThreadScreen from "./screens/Thread";
-import LoginScreen from './screens/Login';
+import LoginScreen from "./screens/Login";
 
-export default createStackNavigator(
-  {
-    Login: LoginScreen,
-    Home: HomeScreen,
-    Thread: {
-      screen: ThreadScreen,
-      navigationOptions: {
-        title: "Thread"
-      }
+const AppStack = createStackNavigator({
+  Home: HomeScreen,
+  Thread: {
+    screen: ThreadScreen,
+    navigationOptions: {
+      title: "HOME"
     }
+  }
+});
+
+const AuthStack = createStackNavigator({
+  Login: LoginScreen
+});
+
+export default createSwitchNavigator(
+  {
+    App: AppStack,
+    Auth: AuthStack,
   },
   {
-    initialRouteName: "Login"
+    initialRouteName: "Auth"
   }
 );
